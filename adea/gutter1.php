0e5725e44ac39e75fbac0683f6e75375154a2977
Adea <blayadea@gmail.com>
2019-12-13 11:56:52 +0000

    Reverse String and Print Numbers

---
+++ adea/gutter1.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Reverse String</title>
+    <meta charset="utf-8">
+  </head>
+  
+
+  <body>
+<h1> Reverse this String</h1>
+
+<?php function numberPlay($someNumber){
+
+for ($i=1; $i<=$someNumber; $i++){
+    if($i % 5 ==0 && $i % 3 ==0){
+        echo "fizzbuzz"; 
+     }else if ($i % 5 == 0){
+         echo "buzz";
+        }
+         else if($i % 3 == 0){
+         echo "fizz";
+         }
+    }
+ }
+
+//split string into an array
+
+//loop through the string
+
+ ?>
+  </body>
+</html>
---
+++ adea/gutter2.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Reverse String</title>
+    <meta charset="utf-8">
+  </head>
+  
+
+  <body>
+<h1>Print Nun String/h1>
+
+<?php function stringReverse($someString){
+
+for ($i=1; $i<=$someNumber; $i++){
+    if($i % 5 ==0 && $i % 3 ==0){
+        echo "fizzbuzz"; 
+     }else if ($i % 5 == 0){
+         echo "buzz";
+        }
+         else if($i % 3 == 0){
+         echo "fizz";
+         }
+    }
+ }
+
+//split string into an array
+
+//loop through the string
+
+ ?>
+  </body>
+</html>
