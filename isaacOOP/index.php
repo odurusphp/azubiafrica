7bee4ed64edc86e6d8d263b443348c472cfb2a85
Isaac <isaac@getinnotized.com>
2020-01-17 11:05:10 +0000

    OOP Assignment

---
+++ isaacOOP/index.php
@@ -0,0 +1,37 @@
+<?php
+
+class Calculations {
+
+    public $score = 89;
+
+    public static function TotalScore($score) {
+        return $score;
+    }
+
+    public function AverageScore() {
+
+    }
+
+}
+
+
+class StudentRanking extends Calculations {
+    public function  __construct(){
+        $this->TotalScore($this->score);
+    }
+}
+
+
+
+function Ranking() {
+
+    $studentscores =   [77, 40 , 89, 65, 100, 55,  70, 80, 33, 85, 96, 88];
+    sort($studentscores);
+
+    $lgth = count($studentscores);
+    for($x = 0; $x < $lgth; $x++) {
+        echo $studentscores[$x].",";
+    }
+}
+
+echo Ranking();
